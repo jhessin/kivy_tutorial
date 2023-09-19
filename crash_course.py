c18f1d2190611ae9ee8236958b46b24e12da1e19
Jim Hessin <jhessin@gmail.com>
2023-09-19 16:34:34 -0600

    Graphics

--- crash_course.py
+++ crash_course.py
@@ -5,13 +5,17 @@ from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
+
+from kivy.properties import ListProperty
+
 import random
 
 class ScatterTextWidget(BoxLayout):
+    text_color = ListProperty([0, 0, 1, 1])
+
     def change_label_color(self, *args):
         color = [random.random() for i in range(3)] + [1]
-        label = self.ids['my_label']
-        label.color = color
+        self.text_color = color
 
 class TutorialApp(App):
     def build(self):
